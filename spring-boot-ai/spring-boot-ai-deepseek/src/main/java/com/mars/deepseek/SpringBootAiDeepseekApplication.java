2d3260e4a5eb8f266ab62ed597b90eb670f689f2
IceYuanyyy <2478686497@qq.com>
2025-03-22 16:37:37 +0800

    Add files via upload

---
+++ spring-boot-ai/spring-boot-ai-deepseek/src/main/java/com/mars/deepseek/SpringBootAiDeepseekApplication.java
@@ -0,0 +1,13 @@
+package com.mars.deepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiDeepseekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAiDeepseekApplication.class, args);
+    }
+
+}
